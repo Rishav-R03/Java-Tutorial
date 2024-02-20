267cbbd7cd97e2df521ba38d412ca53e181af3b9
Rishav-R03 <rishav042023@gmail.com>
2024-02-20 15:16:25 +0530

    java programs

---
+++ Constants.java
@@ -0,0 +1,11 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class HelloWorld {
+    public static void main(String[] args) {
+        final double pi = 3.14;
+        double inc  = pi + 10;
+        System.out.println(pi);
+        System.out.println(inc);
+    }
+}
--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,17 @@
-class HelloWorld{
-    public static void main(String[] args){
-        System.out.println("Hello,World");
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.Scanner;
+
+class HelloWorld {
+    public static void main(String[] args) {
+        int num;
+        Scanner s = new Scanner(System.in);
+        num = s.nextInt();
+        for(int i = 1;i<11;i++){
+            if(i%2==0){
+            System.out.println(num*i);
+                break;
+            }
+        }
     }
 }
