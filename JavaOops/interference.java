7f80999ed0582deb733902f02b975e6d16882870
Rishav-R03 <rishav042023@gmail.com>
2024-03-21 23:56:16 +0530

    interface question q2

---
+++ JavaOops/interference.java
@@ -0,0 +1,24 @@
+package JavaOops;
+
+/**
+ * Innerinterference
+ */
+interface Person{
+    public void displayName();
+    public void displayAge();
+}
+class person implements Person{
+      public void displayName(){
+        System.out.println("Rishav");
+      }
+      public void displayAge(){
+        System.out.println(21);
+      }
+}
+class Main{
+    public static void main(String [] args){
+        person ob = new person();
+        ob.displayName();
+        ob.displayAge();
+    }
+}
