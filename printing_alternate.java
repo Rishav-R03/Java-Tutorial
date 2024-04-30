8e5dbf4c7822be498998eaac09bb4a8e8cae5b93
Rishav-R03 <rishav042023@gmail.com>
2024-04-30 13:59:40 +0530

    Printing even odd simultaneously

---
+++ printing_alternate.java
@@ -0,0 +1,28 @@
+class printing_alternate {
+    public static void main(String [] args){
+        Thread evenThread = new Thread(()->{
+            for(int i = 2;i<=10;i+=2){
+                System.out.println(i);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        Thread oddThread = new Thread(()->{
+            for(int i = 1;i<=9;i+=2){
+                System.out.println(i);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        evenThread.start();
+        oddThread.start();
+    }
+   
+}
