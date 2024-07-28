b84adc0fcc2d7da902a362756b3a8b838bd76917
Rishav-R03 <rishav042023@gmail.com>
2024-07-28 23:53:21 +0530

    rectangle

---
+++ New folder/rectangle.java
@@ -0,0 +1,14 @@
+/**
+ * rectangle
+ */
+public class rectangle {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 4; i++) {
+            System.out.println("*");
+            for (int j = 1; j <= 5; j++) {
+                System.out.print("*");
+            }
+        }
+    }
+
+}
