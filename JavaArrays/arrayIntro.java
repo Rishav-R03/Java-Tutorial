21968f564943bb5a6a79c556d984a2df6a9c1ddc
Rishav-R03 <rishav042023@gmail.com>
2024-08-05 21:49:02 +0530

    arrays revisiion

---
+++ JavaArrays/arrayIntro.java
@@ -0,0 +1,18 @@
+package JavaArrays;
+
+// package Javarrays;
+import java.util.*;
+
+public class arrayIntro {
+    // type[] arrayname = new type[5]; type can be int,char
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+        arr[0] = 1;
+        arr[1] = 3;
+        arr[2] = 2;
+        arr[3] = 4;
+        arr[4] = 5;
+
+        System.out.println(arr[0]);
+    }
+}
